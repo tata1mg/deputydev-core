d75cc071623eac83527477303bb0e8f2836eee4c
sachendra jain <sachendra.jain@1mg.com>
2025-02-13 15:59:31 +0530

    core-completed

---
+++ services/repo/local_repo/local_repo_factory.py
@@ -0,0 +1,21 @@
+from git import InvalidGitRepositoryError, Repo
+
+from common.services.repo.local_repo.managers.base_local_repo_service import BaseLocalRepo
+from common.services.repo.local_repo.managers.git_repo_service import GitRepo
+from common.services.repo.local_repo.managers.non_vcs_repo_service import NonVCSRepo
+
+
+class LocalRepoFactory:
+    @classmethod
+    def _is_git_repo(cls, repo_path: str) -> bool:
+        try:
+            Repo(path=repo_path)
+            return True
+        except InvalidGitRepositoryError:
+            return False
+
+    @classmethod
+    def get_local_repo(cls, repo_path: str) -> BaseLocalRepo:
+        if cls._is_git_repo(repo_path):
+            return GitRepo(repo_path)
+        return NonVCSRepo(repo_path)
