39acdb66e7c014718a541dd7a6d5927808b11c57
sachendra jain <sachendra.jain@1mg.com>
2025-02-13 15:59:31 +0530

    core-completed

---
+++ utils/constants.py
@@ -0,0 +1,42 @@
+from enum import Enum
+
+APP_VERSION = "1.0.4"
+LARGE_NO_OF_CHUNKS = 60
+JAVASCRIPT_EXTENSIONS = {
+    "js": "javascript",
+    "jsx": "javascript",
+    "mjs": "javascript",
+    "cjs": "javascript",
+    "es": "javascript",
+    "es6": "javascript",
+}
+EXTENSION_TO_LANGUAGE = {"py": "python", "html": "html", "kt": "kotlin", "go": "go", "json": "json"}
+TSX_EXTENSIONS = {"tsx": "tsx"}
+JAVA_EXTENSIONS = {"java": "java"}
+TYPESCRIPT_EXTENSIONS = {
+    "ts": "typescript",
+    "mts": "typescript",
+    "cts": "typescript",
+}
+
+ALL_EXTENSIONS = {
+    **JAVASCRIPT_EXTENSIONS,
+    **TYPESCRIPT_EXTENSIONS,
+    **JAVA_EXTENSIONS,
+    **TSX_EXTENSIONS,
+    **EXTENSION_TO_LANGUAGE,
+}
+
+
+class ExtendedEnum(Enum):
+    @classmethod
+    def list(cls):
+        return list(map(lambda c: c.value, cls))
+
+
+class LLMModelNames(ExtendedEnum):
+    GPT_3_5_TURBO = "gpt-3.5-turbo"
+    GPT_4 = "gpt-4"
+    GPT_4_PREVIEW = "gpt-4-1106-preview"
+    GPT_4_O = "gpt-4o"
+    GPT_TEXT_EMBEDDING_3_SMALL = "text-embedding-3-small"
