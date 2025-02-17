2d5fcf48037bc2279b19741d0d1ed804f0f95718
Pratham verma <pratham.verma@1mg.com>
2025-02-17 20:16:24 +0530

    auth token changes

---
+++ deputydev_core/services/auth_token_storage/cli_auth_token_storage_manager.py
@@ -0,0 +1,22 @@
+from services.auth_token_storage.base_auth_token_storage_manager import (
+    AuthTokenStorageBase,
+)
+from utils.constants import AuthTokenKeys
+
+
+class CLIAuthTokenStorageManager(AuthTokenStorageBase):
+    """
+    A class to manage authentication tokens specifically for the command-line interface (CLI)
+    using a persistent storage mechanism.
+
+    Inherits from AuthTokenStorageBase and defines the key_name for storing
+    and retrieving the authentication token.
+
+    Attributes:
+        key_name (str): The name of the key used to store the authentication token,
+                        derived from the AuthTokenKeys. This key is essential
+                        for ensuring that the correct token is accessed and
+                        managed within the persistent storage.
+    """
+
+    key_name = AuthTokenKeys.CLI_AUTH_TOKEN.value
