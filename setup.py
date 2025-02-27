e9cbde9f8dc48bbbbcb5d24b5542068b02e1a1ea
Satyam Kumar <satyam.kumar1@1mg.com>
2025-02-27 21:34:28 +0530

    DD-403 - Minor fix

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ dependency_links = []
 
 setup(
     name="deputydev-core",
-    version="0.0.2",
+    version="0.0.3",
     author="1mg",
     author_email="devops@1mg.com",
     description="Core logic of deputydev",
