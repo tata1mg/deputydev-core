e91205c3aecd8c866a7ce94f2e07d5a9eee822c3
Anand Raj <anand.raj@1mg.com>
2025-03-12 13:42:02 +0530

    upgraded setup

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ dependency_links = []
 
 setup(
     name="deputydev-core",
-    version="0.0.6",
+    version="0.0.7",
     author="1mg",
     author_email="devops@1mg.com",
     description="Core logic of deputydev",
