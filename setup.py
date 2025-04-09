8abc1e11f1afb91d7ad3b551ab2d40d263ad6b4b
Satyam Kumar <satyam.kumar1@1mg.com>
2025-04-09 12:34:45 +0530

    DD-428 - Fix weaviate binary path

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ dependency_links = []
 
 setup(
     name="deputydev-core",
-    version="0.2.5",
+    version="1.0.0",
     author="1mg",
     author_email="devops@1mg.com",
     description="Core logic of deputydev",
