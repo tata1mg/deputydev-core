2994609eb36f4ce463d103f1ca4c7725d8ce7c77
deepaks-1mg <deepak.sharma1@1mg.com>
2025-04-24 20:45:41 +0530

    windows support

--- deputydev_core/utils/shared_memory.py
+++ deputydev_core/utils/shared_memory.py
@@ -2,7 +2,7 @@ import multiprocessing
 import pickle
 from multiprocessing import shared_memory
 from typing import Any
-
+from deputydev_core.utils.context_vars import set_context_values, get_context_value
 
 class SharedMemory:
     _locks = {}  # Dictionary for key-based locks
@@ -16,38 +16,46 @@ class SharedMemory:
 
     @classmethod
     def create(cls, shm_name: str, data: Any) -> None:
-        """Creates or overwrites shared memory with pickled data, using a lock."""
-        data_bytes = pickle.dumps(data)  # Serialize object
-
-        with cls._get_lock(shm_name):  # Ensure only one process writes at a time
-            try:
-                existing_shm = shared_memory.SharedMemory(name=shm_name, create=False)
-                if len(data_bytes) > existing_shm.size:
-                    # if Existing shared memory too small. Recreating...")
-                    existing_shm.close()
-                    existing_shm.unlink()
-                    raise FileNotFoundError
-                # Overwriting existing shared memory...
-                shm = existing_shm
-            except FileNotFoundError:
-                # Creating new shared memory...
-                shm = shared_memory.SharedMemory(name=shm_name, create=True, size=len(data_bytes))
-
-            shm.buf[: len(data_bytes)] = data_bytes  # Write serialized data
-            shm.close()
+        set_context_values(**{shm_name: data})
 
     @classmethod
     def read(cls, shm_name: str) -> Any:
-        """Reads from shared memory safely using a lock."""
-        with cls._get_lock(shm_name):  # Ensure safe access while reading
-            try:
-                shm = shared_memory.SharedMemory(name=shm_name, create=False)
-                raw_data = bytes(shm.buf[:])
-                data = pickle.loads(raw_data)  # Deserialize object
-                shm.close()
-                return data
-            except FileNotFoundError:
-                return None
+        return get_context_value(shm_name)
+
+    # @classmethod
+    # def create(cls, shm_name: str, data: Any) -> None:
+    #     """Creates or overwrites shared memory with pickled data, using a lock."""
+    #     data_bytes = pickle.dumps(data)  # Serialize object
+    #
+    #     with cls._get_lock(shm_name):  # Ensure only one process writes at a time
+    #         try:
+    #             existing_shm = shared_memory.SharedMemory(name=shm_name, create=False)
+    #             if len(data_bytes) > existing_shm.size:
+    #                 # if Existing shared memory too small. Recreating...")
+    #                 existing_shm.close()
+    #                 existing_shm.unlink()
+    #                 raise FileNotFoundError
+    #             # Overwriting existing shared memory...
+    #             shm = existing_shm
+    #         except FileNotFoundError:
+    #             # Creating new shared memory...
+    #             shm = shared_memory.SharedMemory(name=shm_name, create=True, size=len(data_bytes))
+    #
+    #         shm.buf[: len(data_bytes)] = data_bytes  # Write serialized data
+    #         shm.close()
+    #
+    # @classmethod
+    # def read(cls, shm_name: str) -> Any:
+    #     """Reads from shared memory safely using a lock."""
+    #     with cls._get_lock(shm_name):  # Ensure safe access while reading
+    #         try:
+    #             shm = shared_memory.SharedMemory(name=shm_name, create=False)
+    #             raw_data = bytes(shm.buf[:])
+    #             data = pickle.loads(raw_data)  # Deserialize object
+    #             shm.close()
+    #             return data
+    #         except FileNotFoundError:
+    #             return None
 
     @classmethod
     def delete(cls, shm_name: str) -> None:
