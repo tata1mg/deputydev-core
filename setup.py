a64ba44c83daa0d39b4afd57c45ec3dc074600ba
Deepak <deepak.sharma1@1mg.com>
2025-04-27 23:57:55 +0530

    update version in setup py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ dependency_links = []
 
 setup(
     name="deputydev-core",
-    version="2.0.0",
+    version="temp-0.0.3",
     author="1mg",
     author_email="devops@1mg.com",
     description="Core logic of deputydev",
