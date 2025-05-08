0e9999d2cf9ceaf2ed61edc33379acffaec130dd
deepaks-1mg <50229808+deepaks-1mg@users.noreply.github.com>
2025-05-08 18:04:48 +0530

    Revert "windows support"

--- deputydev_core/services/embedding/base_one_dev_embedding_manager.py
+++ deputydev_core/services/embedding/base_one_dev_embedding_manager.py
@@ -6,7 +6,7 @@ from deputydev_core.services.embedding.base_embedding_manager import (
 )
 from deputydev_core.services.tiktoken import TikToken
 from deputydev_core.utils.app_logger import AppLogger
-from deputydev_core.utils.context_value import ContextValue
+from deputydev_core.utils.shared_memory import SharedMemory
 
 
 class BaseOneDevEmbeddingManager(BaseEmbeddingManager):
@@ -51,7 +51,7 @@ class BaseOneDevEmbeddingManager(BaseEmbeddingManager):
                 payload={"texts": batch, "store_embeddings": store_embeddings},
                 headers={
                     "Content-Type": "application/json",
-                    "Authorization": f"Bearer {ContextValue.get(self.auth_token_key)}",
+                    "Authorization": f"Bearer {SharedMemory.read(self.auth_token_key)}",
                 },
             )
             return (
