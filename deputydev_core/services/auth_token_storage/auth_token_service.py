46155b5f2de9629c5ba01360b3307bf668ed4d33
deepaks-1mg <deepak.sharma1@1mg.com>
2025-05-14 21:03:07 +0530

    Reapply "windows support"
    This reverts commit 72d562d5d4fcd8fb083b682f3fa600e605240ca6.

--- deputydev_core/services/auth_token_storage/auth_token_service.py
+++ deputydev_core/services/auth_token_storage/auth_token_service.py
@@ -6,8 +6,8 @@ from deputydev_core.services.auth_token_storage.cli_auth_token_storage_manager i
 from deputydev_core.services.auth_token_storage.extension_auth_token_storage_manager import (
     ExtensionAuthTokenStorageManager,
 )
-from deputydev_core.utils.constants.enums import Clients, SharedMemoryKeys, Status
-from deputydev_core.utils.shared_memory import SharedMemory
+from deputydev_core.utils.constants.enums import Clients, ContextValueKeys, Status
+from deputydev_core.utils.context_value import ContextValue
 
 
 class AuthTokenService:
@@ -103,8 +103,8 @@ class AuthTokenService:
     @classmethod
     def get_auth_token(cls, client: str) -> str:
         if client == Clients.CLI.value:
-            return SharedMemory.read(SharedMemoryKeys.CLI_AUTH_TOKEN.value)
+            return ContextValue.get(ContextValueKeys.CLI_AUTH_TOKEN.value)
         elif client == Clients.VSCODE_EXT.value:
-            return SharedMemory.read(SharedMemoryKeys.EXTENSION_AUTH_TOKEN.value)
+            return ContextValue.get(ContextValueKeys.EXTENSION_AUTH_TOKEN.value)
         else:
             raise ValueError("Invalid Client")
