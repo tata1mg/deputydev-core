089acc1e97308c8e9f1f4b79b1e2fd81871ac3e6
keshav-sethi-1mg <keshav.sethi@1mg.com>
2025-07-01 17:16:39 +0530

    ruff formatting fixed

--- deputydev_core/utils/constants/constants.py
+++ deputydev_core/utils/constants/constants.py
@@ -6,39 +6,49 @@ LARGE_NO_OF_CHUNKS = 60
 
 ALL_EXTENSIONS = {
     # Python
-    'py': 'python',
+    "py": "python",
     # JavaScript family
-    'js': 'javascript', 'jsx': 'javascript', 'mjs': 'javascript', 'cjs': 'javascript',
+    "js": "javascript",
+    "jsx": "javascript",
+    "mjs": "javascript",
+    "cjs": "javascript",
     # TypeScript family
-    'ts': 'typescript', 'tsx': 'typescript',
+    "ts": "typescript",
+    "tsx": "typescript",
     # Java
-    'java': 'java',
+    "java": "java",
     # C/C++
-    'c': 'c', 'h': 'c',
-    'cpp': 'cpp', 'cc': 'cpp', 'cxx': 'cpp', 'hpp': 'cpp',
+    "c": "c",
+    "h": "c",
+    "cpp": "cpp",
+    "cc": "cpp",
+    "cxx": "cpp",
+    "hpp": "cpp",
     # Other languages
-    'go': 'go',
-    'rs': 'rust',
-    'rb': 'ruby',
-    'php': 'php',
-    'swift': 'swift',
-    'kt': 'kotlin',
-    'scala': 'scala',
-    'css': 'css',
-    'html': 'html', 'htm': 'html',
-    'json': 'json',
-    'yaml': 'yaml', 'yml': 'yaml',
-    'sh': 'bash',
-    'sql': 'sql',
-    'lua': 'lua',
-    'dart': 'dart',
-    'r': 'r',
-    'toml': 'toml',
-    'xml': 'xml',
-    'md': 'markdown',
-    'tex': 'latex',
-    'elm': 'elm',
-    'zig': 'zig',
+    "go": "go",
+    "rs": "rust",
+    "rb": "ruby",
+    "php": "php",
+    "swift": "swift",
+    "kt": "kotlin",
+    "scala": "scala",
+    "css": "css",
+    "html": "html",
+    "htm": "html",
+    "json": "json",
+    "yaml": "yaml",
+    "yml": "yaml",
+    "sh": "bash",
+    "sql": "sql",
+    "lua": "lua",
+    "dart": "dart",
+    "r": "r",
+    "toml": "toml",
+    "xml": "xml",
+    "md": "markdown",
+    "tex": "latex",
+    "elm": "elm",
+    "zig": "zig",
 }
 
 
@@ -99,65 +109,139 @@ class SupportedPlatforms(Enum):
 
 # Special filename mappings
 SPECIAL_FILENAME_MAP: Dict[str, str] = {
-    'dockerfile': 'dockerfile',
-    'makefile': 'make',
-    'gnumakefile': 'make',
-    'cmakelists.txt': 'cmake',
+    "dockerfile": "dockerfile",
+    "makefile": "make",
+    "gnumakefile": "make",
+    "cmakelists.txt": "cmake",
 }
 
 # File type extension sets
 CODE_EXTENSIONS: Set[str] = {
-    '.py', '.js', '.jsx', '.ts', '.tsx', '.java', '.cpp', '.c', '.h', '.hpp',
-    '.go', '.rs', '.rb', '.php', '.swift', '.kt', '.scala', '.css', '.html',
-    '.htm', '.sql', '.lua', '.dart', '.r', '.elm', '.zig', '.sh', '.bash'
+    ".py",
+    ".js",
+    ".jsx",
+    ".ts",
+    ".tsx",
+    ".java",
+    ".cpp",
+    ".c",
+    ".h",
+    ".hpp",
+    ".go",
+    ".rs",
+    ".rb",
+    ".php",
+    ".swift",
+    ".kt",
+    ".scala",
+    ".css",
+    ".html",
+    ".htm",
+    ".sql",
+    ".lua",
+    ".dart",
+    ".r",
+    ".elm",
+    ".zig",
+    ".sh",
+    ".bash",
 }
 
-TEXT_EXTENSIONS: Set[str] = {
-    '.md', '.txt', '.rst', '.tex', '.org', '.adoc', '.textile'
-}
+TEXT_EXTENSIONS: Set[str] = {".md", ".txt", ".rst", ".tex", ".org", ".adoc", ".textile"}
 
 CONFIG_EXTENSIONS: Set[str] = {
-    '.json', '.yaml', '.yml', '.toml', '.ini', '.cfg', '.env', '.xml',
-    '.properties', '.conf', '.config'
+    ".json",
+    ".yaml",
+    ".yml",
+    ".toml",
+    ".ini",
+    ".cfg",
+    ".env",
+    ".xml",
+    ".properties",
+    ".conf",
+    ".config",
 }
 
 BINARY_EXTENSIONS: Set[str] = {
-    '.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff', '.webp', '.svg',
-    '.pdf', '.doc', '.docx', '.xls', '.xlsx', '.ppt', '.pptx',
-    '.zip', '.rar', '.7z', '.tar', '.gz', '.bz2',
-    '.exe', '.dll', '.so', '.dylib', '.bin',
-    '.pyc', '.pyo', '.class', '.jar', '.war',
-    '.mp3', '.mp4', '.avi', '.mov', '.wmv'
+    ".jpg",
+    ".jpeg",
+    ".png",
+    ".gif",
+    ".bmp",
+    ".tiff",
+    ".webp",
+    ".svg",
+    ".pdf",
+    ".doc",
+    ".docx",
+    ".xls",
+    ".xlsx",
+    ".ppt",
+    ".pptx",
+    ".zip",
+    ".rar",
+    ".7z",
+    ".tar",
+    ".gz",
+    ".bz2",
+    ".exe",
+    ".dll",
+    ".so",
+    ".dylib",
+    ".bin",
+    ".pyc",
+    ".pyo",
+    ".class",
+    ".jar",
+    ".war",
+    ".mp3",
+    ".mp4",
+    ".avi",
+    ".mov",
+    ".wmv",
 }
 
 # Tree-sitter node types for code structures
 IMPORTANT_NODE_TYPES: Set[str] = {
-    'class_definition', 'class_declaration', 'class',
-    'function_definition', 'function_declaration', 'function', 'method_definition',
-    'import_statement', 'import_declaration', 'import_from_statement',
-    'decorator', 'annotation',
-    'interface_declaration', 'enum_declaration', 'struct_declaration',
-    'variable_declaration', 'const_declaration', 'let_declaration'
+    "class_definition",
+    "class_declaration",
+    "class",
+    "function_definition",
+    "function_declaration",
+    "function",
+    "method_definition",
+    "import_statement",
+    "import_declaration",
+    "import_from_statement",
+    "decorator",
+    "annotation",
+    "interface_declaration",
+    "enum_declaration",
+    "struct_declaration",
+    "variable_declaration",
+    "const_declaration",
+    "let_declaration",
 }
 
 # Code patterns for regex fallback
 CODE_PATTERNS = [
-    (r'^\s*class\s+\w+', 'class'),
-    (r'^\s*def\s+\w+', 'function'),
-    (r'^\s*function\s+\w+', 'function'),
-    (r'^\s*(import|from)\s+', 'import'),
-    (r'^\s*@\w+', 'decorator'),
-    (r'^\s*(public|private|protected)\s+class\s+\w+', 'class'),
-    (r'^\s*(public|private|protected)?\s*(static)?\s*\w+\s*\(', 'function'),
+    (r"^\s*class\s+\w+", "class"),
+    (r"^\s*def\s+\w+", "function"),
+    (r"^\s*function\s+\w+", "function"),
+    (r"^\s*(import|from)\s+", "import"),
+    (r"^\s*@\w+", "decorator"),
+    (r"^\s*(public|private|protected)\s+class\s+\w+", "class"),
+    (r"^\s*(public|private|protected)?\s*(static)?\s*\w+\s*\(", "function"),
 ]
 
 # Text patterns for markdown/text files
 TEXT_PATTERNS = [
-    (r'^#{1,6}\s+', 'header'),
-    (r'^\s*[-*+]\s+', 'list_item'),
-    (r'^\s*\d+\.\s+', 'ordered_list'),
-    (r'^```', 'code_block'),
-    (r'^\s*>', 'quote'),
+    (r"^#{1,6}\s+", "header"),
+    (r"^\s*[-*+]\s+", "list_item"),
+    (r"^\s*\d+\.\s+", "ordered_list"),
+    (r"^```", "code_block"),
+    (r"^\s*>", "quote"),
 ]
 
 # Configuration limits
@@ -168,4 +252,4 @@ MAX_FILE_SIZE = 50 * 1024 * 1024  # 50MB
 MAX_SIGNATURE_LINES = 3
 
 # Directories to ignore
-IGNORED_PATHS = {'.deputydev', '__pycache__', '.git', 'node_modules', '.venv', 'venv'}
+IGNORED_PATHS = {".deputydev", "__pycache__", ".git", "node_modules", ".venv", "venv"}
